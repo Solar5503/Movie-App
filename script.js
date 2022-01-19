bd0c38cb44d8b4ab052f7948677694b7cee9d576
Solar5503 <spidervvv@mail.ru>
2022-01-19 17:20:57 +0300

    adding movies to the DOM

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
 const API_URL =
-  'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=64c4c14a95c753fc8de3b5cd55a90f57&page=1&language=ru';
+  'https://api.themoviedb.org/3/movie/popular?api_key=64c4c14a95c753fc8de3b5cd55a90f57&language=ru&page=1';
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
 const SEARCH_API =
   'https://api.themoviedb.org/3/search/movie?api_key=64c4c14a95c753fc8de3b5cd55a90f57&language=ru&query="';
 const form = document.getElementById('form');
 const search = document.getElementById('search');
+const main = document.getElementById('main');
 
 async function getMovies(url) {
   const res = await fetch(url);
   const data = await res.json();
 
-  console.log(data.results);
+  showMovies(data.results);
+}
+
+function colorRate(vote) {
+  if (vote >= 8) return 'green';
+  else if (vote >= 5) return 'orange';
+  else return 'red';
+}
+
+function showMovies(movies) {
+  main.innerHTML = '';
+  movies.forEach((movie) => {
+    const { title, poster_path, overview, vote_average } = movie;
+    const html = `<div class="movie">
+            <img src="${IMG_PATH + poster_path}" alt="${title}" />
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${colorRate(vote_average)}">${
+      movie.vote_average
+    }</span>
+            </div>
+            <div class="overview">
+                <h3>Описание</h3>
+                ${overview}
+            </div>
+        </div>`;
+    main.insertAdjacentHTML('beforeend', html);
+  });
 }
 
 form.addEventListener('submit', (e) => {
